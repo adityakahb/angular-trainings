85b5c151039a63d301303bac69e74f6e8515ba59
abhakay <aditya.bhakay@live.com>
2020-06-09 16:44:59 +0530

    Initial setup

--- ecommapp001/client/src/app/common-components/item-teaser/item-teaser.component.ts
+++ ecommapp001/client/src/app/common-components/item-teaser/item-teaser.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, Input, ViewEncapsulation, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input, ViewEncapsulation, ViewChild, ElementRef, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-item-teaser',
@@ -10,18 +10,28 @@ export class ItemTeaserComponent implements OnInit, AfterViewInit {
   @ViewChild('itemtitle_xs') itemtitle_xs: ElementRef;
   @ViewChild('itemtitle_md') itemtitle_md: ElementRef;
   @ViewChild('itemtitle_xl') itemtitle_xl: ElementRef;
+  @ViewChild('itemtitle_xxl') itemtitle_xxl: ElementRef;
 
   @Input() cProps;
   @Input() theme;
 
-  checkdevice = 'mobile';
+  showxs = 'hidden';
+  showmd = 'hidden';
+  showxl = 'hidden';
+  showxxl = 'hidden';
 
-  constructor() { }
+  checkdevice = '';
+
+  constructor(private ngZone: NgZone) { }
 
   ngOnInit() {
     
   }
   ngAfterViewInit() {
+    this.ngZone.runOutsideAngular(() => {
+      this.generateText('xs');
+    });
+    
     // if (this.itemtitle.nativeElement.offsetHeight > 72) {
     //   let innertext = this.itemtitle.nativeElement.innerHTML;
     //   while (this.itemtitle.nativeElement.offsetHeight > 72) {
@@ -34,5 +44,22 @@ export class ItemTeaserComponent implements OnInit, AfterViewInit {
     // setTimeout(() => {
       
     // }, 0);
+  }
+  generateText(str) {
+    this.checkdevice = str;
+    this['show' + str] = '';
+    setTimeout(() => {
+      const elem = (this['itemtitle_' + str] || {}).nativeElement;
+      console.log('=========(elem || {}).offsetHeight', (elem || {}).offsetHeight);
+      if ((elem || {}).offsetHeight > 72) {
+        let innertext = elem.innerHTML;
+        while(elem.offsetHeight > 72) {
+          innertext = innertext.substr(0, innertext.length - 1);
+          elem.innerHTML = innertext;
+        }
+        innertext = innertext.substr(0, innertext.length - 5);
+        elem.innerHTML = innertext + '...';
+      }
+    }, 0);
   }
 }
